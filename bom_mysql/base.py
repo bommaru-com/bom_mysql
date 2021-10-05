dfd1e8c3b40800c0b46a15b42bbaf4ae0fe77729
koaaaaang <koaaaaang@users.noreply.github.com>
2021-10-05 02:22:34 +0000

    ✅ Ready to clone and code.

--- bom_mysql/base.py
+++ bom_mysql/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+bom_mysql base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the bom_mysql project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
